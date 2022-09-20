e5bbbbb6b44174897976177abb5082e803c9943b
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-09-19 21:35:19 -0300

    agregamos maquetado de lista de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import FormTareas from "./components/FormTareas";
+import ListaTareas from "./components/ListaTareas";
+//importar la hoja de estilo de bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
     <>
-    <h1>Lista de tareas a realizar</h1>
+    <Container>
+    <h1 className="display-4 text-center my-4">Lista de tareas a realizar</h1>
     <hr />
+    <FormTareas></FormTareas>
+    <ListaTareas></ListaTareas>
+    </Container>
     </>
     );
 }
---
+++ src/components/FormTareas.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormTareas = () => {
+    return (
+        <div>
+            <p className='text-center'>Formulario de tareas</p>
+        </div>
+    );
+};
+
+export default FormTareas;
