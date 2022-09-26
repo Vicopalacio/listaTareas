7935cddb693d83bc3d799185e57016b221082b5f
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-09-26 17:30:47 -0300

    agregamos las tareas al localstorage

--- src/components/FormTareas.jsx
+++ src/components/FormTareas.jsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
 
 const FormTareas = () => {
+  //buscar las tareas del localstorage si es que existe
+  const tareaLocalStorage = JSON.parse(localStorage.getItem("listaTareas")) || [];
+
+
   const [tarea, setTarea] = useState("");
-  const [arregloTareas, setArregloTareas] = useState([]);
+  const [arregloTareas, setArregloTareas] = useState(tareaLocalStorage);
+
+  //ciclo de vida de un componente
+  useEffect(()=>{
+    // console.log("prueba del cliclo de vida")
+    //guardar el arreglo de las tareas en el localstorage
+    localStorage.setItem("listaTareas", JSON.stringify(arregloTareas));
+  },[arregloTareas]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
